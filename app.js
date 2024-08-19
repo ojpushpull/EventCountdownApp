9f136baf9fb3c233ee5f1190479b8e1c72f05e61
Okera Johnson <okerajohnson@gmail.com>
2024-08-18 20:53:05 -0400

    step1

---
+++ app.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import {
+    View,
+    Text,
+    TextInput,
+    ScrollView,
+    StyleSheet,
+    TouchableOpacity,
+} from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import Modal from "react-native-modal";
+import { styles } from "./Style"
